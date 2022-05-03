3471cdf020afd4550898f30d5aedb2fe1f7b19c5
Yein Rah <yein.rah@gmail.com>
2022-05-03 23:44:12 +0900

    220503 boj

---
+++ Boj/boj25083.java
@@ -0,0 +1,12 @@
+package a220503;
+
+public class boj25083 {
+	public static void main(String[] args) {
+		System.out.print("         ,r'\"7\n" + 
+				"r`-_   ,'  ,/\n" + 
+				" \\. \". L_r'\n" + 
+				"   `~\\/\n" + 
+				"      |\n" + 
+				"      |");
+	}
+}
