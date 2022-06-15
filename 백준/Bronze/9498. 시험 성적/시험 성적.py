aed5eca0e87c1233a7e6be01b7018591c0ea660d
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-16 02:21:21 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+a = input()
+a = int(a)
+if 90 <= a and a <= 100:
+    print("A")
+elif 80 <= a and a < 90:
+    print("B")
+elif 70 <= a and a < 80:
+    print("C")
+elif 60 <= a and a < 70:
+    print("D")
+else:
+    print("F")
