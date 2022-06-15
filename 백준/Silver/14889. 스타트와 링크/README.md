a1268904c9b28faedb3ee8e34ad52ffea1114f0f
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-16 02:30:06 +0900

    [Silver II] Title: 스타트와 링크, Time: 288 ms, Memory: 17380 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/README.md
@@ -0,0 +1,12 @@
+# [Silver II] 스타트와 링크 - 14889 
+
+[문제 링크](https://www.acmicpc.net/problem/14889) 
+
+### 성능 요약
+
+메모리: 17380 KB, 시간: 288 ms
+
+### 분류
+
+백트래킹(backtracking), 브루트포스 알고리즘(bruteforcing)
+
---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+	static int n, res;
+	static boolean[] people;
+	static int[][] arr;
+	static boolean[][] visited;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		res = Integer.MAX_VALUE;
+		people = new boolean[n];
+		arr = new int[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				arr[i][j] = sc.nextInt();
+
+		dfs(0, 0);
+
+		System.out.println(res);
+		sc.close();
+	}
+
+	private static void dfs(int idx, int cnt) {
+		if (idx >= n)
+			return;
+
+		if (cnt == n / 2) {
+			int sum1 = 0;
+			int sum2 = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (people[i] && people[j])
+						sum1 += arr[i][j];
+					if (!people[i] && !people[j])
+						sum2 += arr[i][j];
+				}
+			}
+			res = Math.min(res, Math.abs(sum1 - sum2));
+			return;
+
+		}
+
+		people[idx] = true;
+		dfs(idx + 1, cnt + 1);
+		people[idx] = false;
+		dfs(idx + 1, cnt);
+	}
+}
