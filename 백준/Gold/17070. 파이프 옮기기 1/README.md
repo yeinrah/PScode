83dd4114661e02c19e10f1d79cb7c5d5a2c427e0
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-16 02:31:07 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 228 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/README.md
@@ -0,0 +1,12 @@
+# [Gold V] 파이프 옮기기 1 - 17070 
+
+[문제 링크](https://www.acmicpc.net/problem/17070) 
+
+### 성능 요약
+
+메모리: 14584 KB, 시간: 228 ms
+
+### 분류
+
+다이나믹 프로그래밍(dp), 그래프 이론(graphs), 그래프 탐색(graph_traversal)
+
---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+	static int n, res;
+	static int[][] map;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		map = new int[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				map[i][j] = sc.nextInt();
+
+		res = 0;
+
+		move(0, 1, 0);
+
+		System.out.println(res);
+		sc.close();
+	}
+
+	// dir 012 : 가로세로대각선
+	private static void move(int r, int c, int dir) {
+		if (r == n - 1 && c == n - 1 && map[r][c] != 1) {
+			res++;
+			return;
+		}
+
+		if (dir == 0) {
+			if (isIn(r, c + 1) && map[r][c + 1] == 0)
+				move(r, c + 1, 0);
+			if (isIn(r + 1, c + 1) && map[r][c + 1] == 0 && map[r + 1][c + 1] == 0 && map[r + 1][c] == 0)
+				move(r + 1, c + 1, 2);
+		}
+
+		else if (dir == 1) {
+			if (isIn(r + 1, c) && map[r + 1][c] == 0)
+				move(r + 1, c, 1);
+			if (isIn(r + 1, c + 1) && map[r][c + 1] == 0 && map[r + 1][c + 1] == 0 && map[r + 1][c] == 0)
+				move(r + 1, c + 1, 2);
+		}
+
+		else if (dir == 2) {
+			if (isIn(r, c + 1) && map[r][c + 1] == 0)
+				move(r, c + 1, 0);
+			if (isIn(r + 1, c) && map[r + 1][c] == 0)
+				move(r + 1, c, 1);
+			if (isIn(r + 1, c + 1) && map[r][c + 1] == 0 && map[r + 1][c + 1] == 0 && map[r + 1][c] == 0)
+				move(r + 1, c + 1, 2);
+		}
+	}
+
+	private static boolean isIn(int r, int c) {
+		return 0 <= r && 0 <= c && r < n && c < n;
+	}
+}
