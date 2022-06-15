22a8160f7be74b4ec810005dbeacfa829357604a
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-16 02:17:50 +0900

    [Gold V] Title: 개똥벌레, Time: 884 ms, Memory: 166040 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int h = sc.nextInt();
+		// 석순
+		int[] bottom = new int[h + 1];
+		// 종유석
+		int[] top = new int[h + 1];
+		for (int i = 0; i < n / 2; i++) {
+			bottom[sc.nextInt()]++;
+			top[sc.nextInt()]++;
+		}
+		
+		for (int i = 1; i <= h; i++) {
+			bottom[i] += bottom[i - 1];
+			top[i] += top[i - 1];
+		}
+		
+		int cnt = 0;
+		int min = n;
+		for (int i = 1; i <= h; i++) {
+			int sum = 0;
+			
+			sum += bottom[h] - bottom[i - 1];
+			sum += top[h] - top[h - i];
+			
+			if (min > sum) {
+				min = sum;
+				cnt = 1;
+			}
+			else if (min == sum)
+				cnt++;
+		}
+		System.out.println(min + " " + cnt);
+		sc.close();
+	}
+}
