b0a32af67d80d555f8639f087507251768f8eeb3
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-16 02:11:06 +0900

    [Gold III] Title: 줄 세우기, Time: 1044 ms, Memory: 161856 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] inDegree = new int[n + 1];
+		List<Integer>[] adj = new ArrayList[n + 1];
+		for (int i = 0; i <= n; i++)
+			adj[i] = new ArrayList<>();
+
+		for (int i = 0; i < m; i++) {
+			int st = sc.nextInt();
+			int ed = sc.nextInt();
+			inDegree[ed]++;
+			adj[st].add(ed);
+		}
+
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 1; i <= n; i++)
+			if (inDegree[i] == 0)
+				q.add(i);
+
+		while (!q.isEmpty()) {
+			int curr = q.poll();
+			sb.append(curr + " ");
+
+			for (int i = 0; i < adj[curr].size(); i++) {
+				int next = adj[curr].get(i);
+				if (next != 0) {
+					inDegree[next]--;
+					if (inDegree[next] == 0)
+						q.add(next);
+				}
+			}
+		}
+		System.out.println(sb);
+		sc.close();
+	}
+}
