a54f3f351de569431b79235461e42aa141ac2e6f
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-16 02:27:53 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 184 ms, Memory: 16912 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main { // DP 문제이자 LIS 문제
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+
+		int[] dp = new int[n];
+		for (int i = 0; i < n; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++)
+				if (arr[i] > arr[j] && dp[i] <= dp[j])
+					dp[i] = dp[j] + 1;
+		}
+
+		int max = 0;
+		for (int i = 0; i < n; i++)
+			max = Math.max(max, dp[i]);
+
+		System.out.println(max);
+		sc.close();
+	}
+}
