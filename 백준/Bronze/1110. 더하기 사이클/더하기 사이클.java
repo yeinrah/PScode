dab9ffd99320082bbb63994f2c8a3dfc2b6e0368
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-16 02:02:01 +0900

    [Bronze I] Title: 더하기 사이클, Time: 108 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int res = 1;
+		
+		int next1 = n % 10;
+		int next2 = (n / 10 + next1) % 10;
+		int tmp = next1 * 10 + next2;
+		
+		while (tmp != n) {
+			next1 = tmp % 10;
+			next2 = (tmp / 10 + next1) % 10;
+			tmp = next1 * 10 + next2;
+			res++;
+		}
+		
+		System.out.println(res);
+		sc.close();
+	}
+}
