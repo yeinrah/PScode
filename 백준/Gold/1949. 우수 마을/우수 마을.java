eb62c1a069cff4982a293668d50489ad97ae5f6f
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-16 02:09:02 +0900

    [Gold II] Title: 우수 마을, Time: 452 ms, Memory: 43128 KB -BaekjoonHub

---
+++ 백준/Gold/1949. 우수 마을/우수 마을.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	static int n;
+	static int[] people;
+	static int[][] dp;
+	static ArrayList<Integer>[] graph;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		people = new int[n + 1];
+		graph = new ArrayList[n + 1];
+		dp = new int[n + 1][2];
+
+		for (int i = 0; i <= n; i++)
+			graph[i] = new ArrayList<>();
+
+		for (int i = 0; i < n; i++)
+			// 정점 번호와 인덱스를 맞춰주기 위해 i + 1
+			people[i + 1] = sc.nextInt();
+		
+		for (int i = 0; i < n - 1; i++) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			graph[start].add(end);
+			graph[end].add(start);
+		}
+		
+		dfs(1, 0);
+		
+		System.out.println(Math.max(dp[1][0], dp[1][1]));
+		sc.close();
+	}
+
+	private static void dfs(int n, int parent) {
+		for (int node : graph[n]) {
+			if (node != parent) {
+				dfs(node, n);
+				dp[n][0] += Math.max(dp[node][0], dp[node][1]);
+				dp[n][1] += dp[node][0];
+			}
+		}
+		dp[n][1] += people[n];
+	}
+}
