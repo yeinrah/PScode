19368084675f5dc0716ebb5c75855a0323c72e7b
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-16 02:11:45 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 112 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[9];
+		int sum = 0;
+		for (int i = 0; i < 9; i++) {
+			arr[i] = sc.nextInt();
+			sum += arr[i];
+		}
+		Arrays.sort(arr);
+		int ex1 = 0;
+		int ex2 = 0;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (sum - arr[j] - arr[i] == 100) {
+					ex1 = j;
+					ex2 = i;
+					break;
+				}
+			}
+		}
+		for (int i = 0; i < 9; i++) {
+			if (i == ex1 || i == ex2)
+				continue;
+			else
+				System.out.println(arr[i]);
+		}
+		sc.close();
+	}
+}
