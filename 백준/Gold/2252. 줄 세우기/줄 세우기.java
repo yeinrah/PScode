525299c1ffc2786d406e1a4ccf79b7bcdcef19b1
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-06-23 20:47:28 +0900

    [Gold III] Title: 줄 세우기, Time: 1012 ms, Memory: 161840 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -5,7 +5,6 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
