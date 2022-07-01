df9fe5f39100659edf02b561f1920c56a3644612
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-07-01 18:35:07 +0900

    [Silver II] Title: 차이를 최대로, Time: 356 ms, Memory: 13792 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,42 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
+	static int n, res;
 	static int[] arr;
-	static int n, max;
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		arr = new int[n];
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++)
 			arr[i] = sc.nextInt();
-		}
-		
-		max = 0;
-		
+
 		perm(0);
-		
-		System.out.println(max);
+
+		System.out.println(res);
+		sc.close();
 	}
-	
-	public static void perm(int idx) {
+
+	private static void perm(int idx) {
+
 		if (idx == n) {
-			int tmp = 0;
-			for (int i = 0; i < n - 1; i++) {
-				tmp += Math.abs(arr[i] - arr[i + 1]);
-			}
-			max = Math.max(tmp, max);
+			int sum = 0;
+			for (int i = 0; i < n - 1; i++)
+				sum += Math.abs(arr[i] - arr[i + 1]);
+
+			res = Math.max(sum, res);
 			return;
 		}
-		
 		for (int i = 0; i < n; i++) {
 			swap(i, idx);
 			perm(idx + 1);
 			swap(i, idx);
 		}
 	}
-	
-	public static void swap(int a, int b) {
+
+	private static void swap(int a, int b) {
 		int tmp = arr[a];
 		arr[a] = arr[b];
 		arr[b] = tmp;
