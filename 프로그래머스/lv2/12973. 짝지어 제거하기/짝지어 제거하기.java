c2dedccca4c8d1965faeed7fb625677ed6745d6d
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-11-25 16:51:22 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.29 ms, Memory: 58.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+        
+        Stack<Character> st = new Stack<Character>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (st.isEmpty() || st.peek() != s.charAt(i))
+                st.push(s.charAt(i));
+            else
+                st.pop();
+        }
+        if (st.isEmpty())
+            answer = 1;
+
+        return answer;
+    }
+}
