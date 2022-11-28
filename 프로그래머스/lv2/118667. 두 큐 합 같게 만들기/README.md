a324b177605dce673e47fa90223e421c8a21bded
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-11-28 13:57:21 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 51.39 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
 
 ### 성능 요약
 
-메모리: 114 MB, 시간: 79.28 ms
+메모리: 121 MB, 시간: 51.39 ms
 
 ### 구분
 
--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -4,6 +4,9 @@ import java.util.*;
 // 질문하기 글들을 참고해서 sum1만 갱신하면 for문 돌릴 시간을 아낄 수 있다는 점을 알아냄
 // 다만, 11, 28번은 sum1 갱신만으로는 풀리지 않는 반례인 것으로 추정, 무한루프
 // 주어진 큐의 길이 제한이 30만이기 때문에 최대 60만번만 돌게 제한하여 해결
+
+// *수정사항*
+// 투 포인터의 최대 이동범위는 전체 큐 사이즈 * 2 이므로 60만 대신 해당 제한을 걸어주면 해결
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         int answer = 0;
@@ -38,17 +41,15 @@ class Solution {
 		if (sum % 2 == 0) {
 			int index1 = 0;
 			int index2 = queue1.length;
-            int cnt = 0;
-			while (cnt <= 600000 && sum1 != sum / 2) {
-                while (cnt <= 600000 && sum1 > sum / 2) {
+            int size = arr.size() * 2;
+			while (answer < size && sum1 != sum / 2) {
+                if (sum1 > sum / 2) {
                     sum1 -= arr.get(index1++ % arr.size());
                     answer++;
-                    cnt++;
                 }
-                while (cnt <= 600000 && sum1 < sum / 2) {
+                if (sum1 < sum / 2) {
                     sum1 += arr.get(index2++ % arr.size());
                     answer++;
-                    cnt++;
                 }
                 if (sum1 == sum / 2)
                     return answer;
