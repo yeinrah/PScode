3c50bd77f71eedcf6a2d20efca42e8d6f80ee5de
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2022-11-30 17:53:08 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int angle) {
+        if (0 < angle && angle < 90)
+            return 1;
+        if (angle == 90)
+            return 2;
+        if (90 < angle && angle < 180)
+            return 3;
+        else
+            return 4;
+    }
+}
