b0760297a906f33c1fbbfbfc81a0f724bf596c57
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2023-02-08 20:26:18 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int even = 0;
+        int odd = 0;
+        for (int i : num_list) {
+            if (i % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+        int[] answer = {even, odd};
+        return answer;
+    }
+}
