ebf4171c3aed542e2d7006e3cb05583c5276dfbe
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2023-02-16 21:49:11 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.41 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+        Arrays.sort(weights);
+        
+        int tmp = 0;
+        for (int i = 0; i < weights.length - 1; i++) {
+            if (i > 0 && weights[i] == weights[i - 1]) {
+                tmp--;
+                answer += tmp;
+                continue;
+            }
+            tmp = 0;
+            
+            for (int j = i + 1; j < weights.length; j++) {
+                if (weights[i] == weights[j] ||
+                    weights[i] * 2 == weights[j] * 3 ||
+                    weights[i] * 2 == weights[j] * 4 ||
+                    weights[i] * 3 == weights[j] * 4 ||
+                    weights[i] * 4 == weights[j] * 3 ||
+                    weights[i] * 4 == weights[j] * 2 ||
+                    weights[i] * 3 == weights[j] * 2)
+                    tmp++;
+            }
+            answer += tmp;
+        }
+        return answer;
+    }
+}
