0ed993ab5f612b4af54b83c47300e9ab2ca295dd
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2023-03-31 23:59:20 +0900

    [unrated] Title: 과제 진행하기, Time: 7.14 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.java
@@ -1,48 +1,78 @@
 import java.util.*;
 
 class Solution {
-   static class Task {
-       String name;
-       int startTime;
-       int remainingTime;
-       
-       public Task(String[] info) {
-           name = info[0];
-           String[] split = info[1].split(":");
-           startTime = Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
-           remainingTime = Integer.parseInt(info[2]);
+    static class Task {
+        String name;
+        int remainingTime;
+        
+        public Task(String name, int remainingTime) {
+            this.name = name;
+            this.remainingTime = remainingTime;
         }
     }
-    
     public String[] solution(String[][] plans) {
-        PriorityQueue<Task> plan = new PriorityQueue<>((a, b) -> Integer.compare(a.startTime, b.startTime));
-        Stack<Task> stack = new Stack<>();
-
-        for (String[] p : plans)
-            plan.add(new Task(p));
-
         String[] answer = new String[plans.length];
+        Stack<Task> stack = new Stack<>();
+        Arrays.sort(plans, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] a, String[] b) {
+                int hourDiff = Integer.parseInt(a[1].substring(0, 2)) - Integer.parseInt(b[1].substring(0, 2));
+                int minDiff = Integer.parseInt(a[1].substring(3, 5)) - Integer.parseInt(b[1].substring(3, 5));
+                if (hourDiff < 0 || (hourDiff == 0 && minDiff < 0))
+                    return -1;
+                else
+                    return 1;
+            }
+        });
+        
         int idx = 0;
-        int currTime = 0;
-
-        while (!plan.isEmpty() || !stack.isEmpty()) {
-            if (plan.isEmpty())
-                answer[idx++] = stack.pop().name;
-            else if (stack.isEmpty()) {
-                currTime = plan.peek().startTime;
-                stack.push(plan.poll());
+        for (int i = 0; i < plans.length; i++) {
+            String name = plans[i][0];
+            int hour = Integer.parseInt(plans[i][1].substring(0, 2));
+            int min = Integer.parseInt(plans[i][1].substring(3, 5));
+            int playtime = Integer.parseInt(plans[i][2]);
+            
+            if (i == plans.length - 1) {
+                stack.add(new Task(name, playtime));
+                break;
+            }
+            
+            int nextHour = Integer.parseInt(plans[i + 1][1].substring(0, 2));
+            int nextMin = Integer.parseInt(plans[i + 1][1].substring(3, 5));
+            int timeDiff = (nextHour - hour) * 60 + (nextMin - min);
+            if (timeDiff < playtime)
+                stack.add(new Task(name, playtime - timeDiff));
+            else if (timeDiff > playtime) {
+                answer[idx] = name;
+                idx++;
+                if (!stack.isEmpty()) {
+                    int time = timeDiff - playtime;
+                    while (time > 0) {
+                        if (stack.isEmpty())
+                            break;
+                        Task task = stack.pop();
+                        if (task.remainingTime <= time) {
+                            time -= task.remainingTime;
+                            answer[idx] = task.name;
+                            idx++;
+                        }
+                        else {
+                            task.remainingTime -= time;
+                            time -= time;
+                            stack.add(new Task(task.name, task.remainingTime));
+                        }
+                    }
+                }
             }
             else {
-                if (plan.peek().startTime < currTime + stack.peek().remainingTime) {
-                    stack.peek().remainingTime -= plan.peek().startTime - currTime;
-                    currTime = plan.peek().startTime;
-                    stack.push(plan.poll());
-                }
-                else {
-                    currTime += stack.peek().remainingTime;
-                    answer[idx++] = stack.pop().name;
-                }
+                answer[idx] = name;
+                idx++;
             }
+        }
+        while (!stack.isEmpty()) {
+            Task task = stack.pop();
+            answer[idx] = task.name;
+            idx++;
         }
         return answer;
     }
