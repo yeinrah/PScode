d61a1a4ef682efa4586d4e01811ea347dfa258e6
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2023-05-04 14:13:38 +0900

    [level 2] Title: 숫자의 표현, Time: 1.65 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1; // 자기 자신
+        int answer = 1;
         
         for (int i = 1; i <= n / 2; i++) {
             int sum = 0;
