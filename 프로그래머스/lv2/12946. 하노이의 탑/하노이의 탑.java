a395203cfe40412fc42cec8d20870a2bf1d5caaa
Yein Rah <63994962+yeinrah@users.noreply.github.com>
2023-05-05 22:51:41 +0900

    [level 2] Title: 하노이의 탑, Time: 2.98 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,21 @@
+class Solution {
+    static int idx = 0;
+    
+    public int[][] solution(int n) {
+        int[][] answer = new int[(int) Math.pow(2, n) - 1][2];
+
+        hanoi(n, 1, 3, 2, answer);
+
+        return answer;
+    }
+
+    public void hanoi(int n, int start, int dest, int waypoint, int[][] answer) {
+        if (n == 1)
+            answer[idx++] = new int[] {start, dest};
+        else {
+            hanoi(n - 1, start, waypoint, dest, answer);
+            answer[idx++] = new int[] {start, dest};
+            hanoi(n - 1, waypoint, dest, start, answer);
+        }
+    }
+}
